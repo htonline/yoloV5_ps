186576bc8a13b478195a422548581661a7b56946
htonline <986344867@qq.com>
2023-04-25 16:24:49 +0800

    yolo.py文件 进度100%

--- models/yolo.py
+++ models/yolo.py
@@ -168,41 +168,73 @@ class BaseModel(nn.Module):
 # Model
 class DetectionModel(BaseModel):
     # YOLOv5 detection model
+    # ch: 输入的通道数; 图片有RGB三个通道, 所以这里通常就是3.
+    # nc: 模型能够检测出来的目标类别, 默认是空;
+    # anchors: 模型所使用的anchors
+    # 如果在初始化过程中, 传入了nc和anchors这两个参数, 则它们会覆盖掉yolov5s.yaml中定义的值
     def __init__(self, cfg='yolov5s.yaml', ch=3, nc=None, anchors=None):  # model, input channels, number of classes
+        # 初始化函数分为4部分:
+
+        """1.加载传入的配置文件"""
         super().__init__()
-        if isinstance(cfg, dict):
+        if isinstance(cfg, dict):               # 判断传入的参数是不是字典类型的值
             self.yaml = cfg  # model dict
         else:  # is *.yaml
-            import yaml  # for torch hub
-            self.yaml_file = Path(cfg).name
-            with open(cfg, encoding='ascii', errors='ignore') as f:
+            import yaml  # for torch hub        导入yaml的python库,加载.yaml格式的文件
+            self.yaml_file = Path(cfg).name     # 获取文件名
+            with open(cfg, encoding='ascii', errors='ignore') as f:     # 加载文件
                 self.yaml = yaml.safe_load(f)  # model dict
+                # 最终self.yaml中, 会以字典<key, value>的形式, 存放yolov5s.yaml中的数据.
+
 
         # Define model
-        ch = self.yaml['ch'] = self.yaml.get('ch', ch)  # input channels
+        """2.利用加载好的配置文件, 来一步步搭建网络的每一层"""
+        # 取出字典中,关键字'ch'所对应的值; 如果没有,就用后面的值作为返回值;后面是作为函数参数传入的值
+        # 同时也是给self.yaml中添加了一个键值对<ch, 3>
+        ch = self.yaml['ch'] = self.yaml.get('ch', ch)  # input channels ------3
+
+        # 如果新传入的参数nc和anchors, 与yaml中的值不一样, 则覆盖上去;
         if nc and nc != self.yaml['nc']:
             LOGGER.info(f"Overriding model.yaml nc={self.yaml['nc']} with nc={nc}")
             self.yaml['nc'] = nc  # override yaml value
         if anchors:
             LOGGER.info(f'Overriding model.yaml anchors with anchors={anchors}')
             self.yaml['anchors'] = round(anchors)  # override yaml value
+
+        # 利用yaml文件, 去搭建网络的每一层, 最后得到yolov5的模型
+        # self.yaml: 配置文件
+        # ch: 通道数
+        # self.model 建好的模型
+        # self.save 需要保存的层数
         self.model, self.save = parse_model(deepcopy(self.yaml), ch=[ch])  # model, savelist
+        # 给每一个类赋一个类名(0:人; 1:汽车; ... )
         self.names = [str(i) for i in range(self.yaml['nc'])]  # default names
+        # 从yaml文件中获取inplace关键字对应的值, 如果没有的话,就返回True;
         self.inplace = self.yaml.get('inplace', True)
 
         # Build strides, anchors
-        m = self.model[-1]  # Detect()
-        if isinstance(m, (Detect, Segment)):
+        """3.求网络步长和对anchors进行处理 """
+        m = self.model[-1]  # Detect() 取出模块的最后一层
+        if isinstance(m, (Detect, Segment)):    # 判断是不是Detect/Segment
+            """新建了一张空白图片:256x256, 通道数ch=3, 放入模型中, 进行一次前向传播"""
             s = 256  # 2x min stride
             m.inplace = self.inplace
             forward = lambda x: self.forward(x)[0] if isinstance(m, Segment) else self.forward(x)
-            m.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(1, ch, s, s))])  # forward
+            # 会做3次预测（小中大）
+            # 通过输入图片的尺寸/预测的尺寸, 得到stride的大小
+            m.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(1, ch, s, s))])  # forward [8,16,32]
+
+            # 检测传入的anchor顺序对不对（必须由小到大）;不是的话就调整
             check_anchor_order(m)
+
+            # 定义的anchors的尺寸是相对在原始图像上的, 但我们是在最终的特征层上的用anchors;
+            # 最终特征层可能相对于原始图像缩小了8倍, 所以anchors也要相对应的缩小
             m.anchors /= m.stride.view(-1, 1, 1)
             self.stride = m.stride
             self._initialize_biases()  # only run once
 
         # Init weights, biases
+        """4.对网络的参数进行初始化, 以及打印的一些操作"""
         initialize_weights(self)
         self.info()
         LOGGER.info('')
@@ -300,32 +332,59 @@ class ClassificationModel(BaseModel):
         self.model = None
 
 
+# d: yaml文件;
+# ch: 通道数[3]
 def parse_model(d, ch):  # model_dict, input_channels(3)
     # Parse a YOLOv5 model.yaml dictionary
     LOGGER.info(f"\n{'':>3}{'from':>18}{'n':>3}{'params':>10}  {'module':<40}{'arguments':<30}")
     anchors, nc, gd, gw, act = d['anchors'], d['nc'], d['depth_multiple'], d['width_multiple'], d.get('activation')
+
     if act:
         Conv.default_act = eval(act)  # redefine default activation, i.e. Conv.default_act = nn.SiLU()
         LOGGER.info(f"{colorstr('activation:')} {act}")  # print
-    na = (len(anchors[0]) // 2) if isinstance(anchors, list) else anchors  # number of anchors
+
+    na = (len(anchors[0]) // 2) if isinstance(anchors, list) else anchors  # number of anchors anchors的数量3
+
+    # 模型最终输出的通道数: na=3; nc=80; 5=4(待检测目标的坐标信息)+1(检测框内存在目标的概率(置信度))
     no = na * (nc + 5)  # number of outputs = anchors * (classes + 5)
 
+    # layers: 存储下面for创建网络的每一层
+    # save: 标签; 统计某些层的特征是需要保存的(比如第16层的数据, 需要第4层和第15层的数据,所以需要存下来)
+    # c2: 输出的通道数. 每一层都会有一个c1和c2,表示输入和输出;
     layers, save, c2 = [], [], ch[-1]  # layers, savelist, ch out
-    for i, (f, n, m, args) in enumerate(d['backbone'] + d['head']):  # from, number, module, args
+    for i, (f, n, m, args) in enumerate(d['backbone'] + d['head']):  # from:-1, number:1, module:"Conv", args:[64,6,2,2]
+        """判断m:'Conv'是字符串之后,通过eval函数去推断,得到m其实表示的是一个类Conv: models.common.Conv """
         m = eval(m) if isinstance(m, str) else m  # eval strings
+
+        """遍历参数.同上: 如果a是字符串类型,它就会推断a到底是什么类型"""
         for j, a in enumerate(args):
             with contextlib.suppress(NameError):
                 args[j] = eval(a) if isinstance(a, str) else a  # eval strings
 
+        # 求number的实际值; (yolov5s.yaml中,它需要乘上一个深度的倍数,以乘积作为真正的这层层数)
+        # 第一次不用乘, 如果n>1,就乘上gd;
+        # round(): 四舍五入;
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
+        # 判断该层属于什么结构, 执行相应的内容; 比如这里是卷积Conv
         if m in {
                 Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
                 BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x}:
+            # 输入的通道数(3), 输出的通道数(64)
             c1, c2 = ch[f], args[0]
+            # 但c2也需要乘上通道倍数,所以这里判断c2与最终的通道数是否相等,如果不等的话,就乘上通道倍数;
             if c2 != no:  # if not output
+                # c2:64 * 0.5 = 32
+                # 接着会判断这个32是不是8的倍数: 深度学习中,模型每一层的通道数设计成8的倍数,这对GPU计算更加友好;
+                # 所以如果不是,就会强制变成8的倍数
                 c2 = make_divisible(c2 * gw, 8)
 
+            # 将c1,c2以及args中,从第2~最后一个值拼接起来.
+            # 输入通道数,输出通道数(乘过之后的),卷积核尺寸,卷积核步长,padding大小 [3,32,6,2,2]
+            # 可以直接利用args, 与Conv初始化对应起来,进行卷积层的初始化工作.
             args = [c1, c2, *args[1:]]
+
+            """如果是C3层的话"""
+            # 会额外拼接上n的值, C3初始化需要
             if m in {BottleneckCSP, C3, C3TR, C3Ghost, C3x}:
                 args.insert(2, n)  # number of repeats
                 n = 1
@@ -347,17 +406,30 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         else:
             c2 = ch[f]
 
+        # 假如n>1, 根据n的数量初始化这一层模块数量.
+        # 第0层:n=1, 只需要一个卷积模块;
+        # 又比如C3模块, n=2, 它就需要在那一层中有两个C3模块
         m_ = nn.Sequential(*(m(*args) for _ in range(n))) if n > 1 else m(*args)  # module
+
+        # 获取模块名, 如果字符串中有__main__., 就用''替换掉.
         t = str(m)[8:-2].replace('__main__.', '')  # module type
+
+        # 统计第0层的参数量
         np = sum(x.numel() for x in m_.parameters())  # number params
+
+        # 赋值
         m_.i, m_.f, m_.type, m_.np = i, f, t, np  # attach index, 'from' index, type, number params
+        # 打印输出信息
         LOGGER.info(f'{i:>3}{str(f):>18}{n_:>3}{np:10.0f}  {t:<40}{str(args):<30}')  # print
+        # 统计哪些层需要保存
         save.extend(x % i for x in ([f] if isinstance(f, int) else f) if x != -1)  # append to savelist
         layers.append(m_)
+        # 如果是第0层, 需要清空通道数
         if i == 0:
             ch = []
+        # 将当前层的通道数添加进去, 下一层需要取出上一层的输出通道. 按-1索引来取, 来作为这一层的输入通道
         ch.append(c2)
-    return nn.Sequential(*layers), sorted(save)
+    return nn.Sequential(*layers), sorted(save) #需要保存的层数行号:save:[4,6,10,14,17,20,23]
 
 
 if __name__ == '__main__':
